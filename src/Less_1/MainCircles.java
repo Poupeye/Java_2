3832e1f779d727efbcd827f5742da95bd1573c5b
Poupeye <106345339+Poupeye@users.noreply.github.com>
2022-07-20 02:06:21 +0300

    1

--- src/Less_1/MainCircles.java
+++ src/Less_1/MainCircles.java
@@ -1,4 +1,90 @@
 package Less_1;
 
-public class MainCircles {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MainCircles extends JFrame {
+
+    private static final int POS_X = 600;
+    private static final int POS_Y = 200;
+    private static final int WINDOW_WIGHT = 800;
+    private static final int WINDOW_HEIGHT = 600;
+    Sprite[] sprites = new Sprite[1];
+    private int spritesCount;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainCircles();
+            }
+        });
+    }
+
+    private MainCircles() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIGHT, WINDOW_HEIGHT);
+        setTitle("Circles");
+
+        GameCanvas canvas = new GameCanvas(this);
+        canvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    int x = e.getX();
+                    int y = e.getY();
+                    addSprite(new Ball(x,y));
+                } else if (e.getButton() == MouseEvent.BUTTON3) {
+                    removeSprite();
+                }
+
+            }
+        });
+        add(canvas, BorderLayout.CENTER);
+        initApplication();
+        setVisible(true);
+
+    }
+
+    private void removeSprite() {
+        if (spritesCount > 1) {
+            spritesCount--;
+        }
+    }
+
+    private void addSprite(Sprite sprite) {
+        if (spritesCount == sprites.length) {
+            Sprite[] newSprites = new Sprite[sprites.length * 2];
+            System.arraycopy(sprites, 0, newSprites, 0, sprites.length);
+            sprites = newSprites;
+        }
+        sprites[spritesCount] = sprite;
+        spritesCount++;
+    }
+
+    private void initApplication() {
+        addSprite(new Background());
+        addSprite(new Ball());
+    }
+
+    public void onDrawFrame(GameCanvas canvas, Graphics g, float deltaTime) {
+        update(canvas, deltaTime);
+        render(canvas, g);
+    }
+
+    void update(GameCanvas canvas, float deltaTime) {
+        for (int i = 0; i < spritesCount; i++) {
+            sprites[i].update(canvas, deltaTime);
+        }
+    }
+
+    void render(GameCanvas canvas, Graphics g) {
+        for (int i = 0; i < spritesCount; i++) {
+            sprites[i].render(canvas, g);
+        }
+    }
+
+
 }
