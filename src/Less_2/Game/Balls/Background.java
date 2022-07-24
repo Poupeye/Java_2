c8e6618f3714a7fe71d3fbc71ba03a3ebf86a833
Poupeye <106345339+Poupeye@users.noreply.github.com>
2022-07-24 21:28:13 +0300

    two

---
+++ src/Less_2/Game/Balls/Background.java
@@ -0,0 +1,26 @@
+package Less_2.Game.Balls;
+
+import Less_2.Game.common.GameCanvas;
+import Less_2.Game.common.GameObject;
+
+import java.awt.*;
+
+public class Background implements GameObject {
+    private float time;
+    private static final float AMPLITUDE = 255f / 2f;
+    private Color color;
+
+    @Override
+    public void update(GameCanvas canvas, float deltaTime) {
+        time += deltaTime;
+        int red = Math.round(AMPLITUDE + AMPLITUDE * (float) Math.sin(time * 2f));
+        int green = Math.round(AMPLITUDE + AMPLITUDE * (float) Math.sin(time * 3f));
+        int blue = Math.round(AMPLITUDE + AMPLITUDE * (float) Math.sin(time));
+        color = new Color(red, green, blue);
+    }
+
+    @Override
+    public void render(GameCanvas gameCanvas, Graphics g) {
+        gameCanvas.setBackground(color);
+    }
+}
