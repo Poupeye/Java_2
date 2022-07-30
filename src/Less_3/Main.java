351c46c9700b55bf3ddcd12bc77f9f068bf61252
Poupeye <106345339+Poupeye@users.noreply.github.com>
2022-07-30 20:01:33 +0300

    3 переделал

--- src/Less_3/Main.java
+++ src/Less_3/Main.java
@@ -1,27 +1,21 @@
 package Less_3;
-
-
-import com.sun.source.tree.IfTree;
-
 import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String[] strArr = new String[]{"сиськи", "письки", "хер", "дотан", "хз", "сиськи", "заебался", "дотан", "заебался", "сиськи", "сиськи"};
         SumString(strArr);
         PhoneBook book = new PhoneBook();
 
+        book.add("lox","100","1@");
+        book.add("lox2","200","2@");
+        book.add("lox3","300","3@");
+        book.add("lox2","900","9@");
+        book.add("lox2","100","5@");
+
+        System.out.println(book.getPerson("lox2"));
 
     }
-
-
-
-
-
-
-
 
 
 
--- src/Less_3/PhoneBook.java
+++ src/Less_3/PhoneBook.java
@@ -2,30 +2,37 @@ package Less_3;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
 public class PhoneBook {
 
-    public HashMap<String, ArrayList<Person>> entries = new HashMap<>();
+    public Map<String, ArrayList<Person>> entries = new HashMap<>();
 
-    public void add (String name, String phone, String email){
+    public void add(String name, String phone, String email) {
         if (entries.containsKey(name)) {
             ArrayList<Person> persons = entries.get(name);
-            persons.add(new Person(phone,email));
+            persons.add(new Person(phone, email));
         } else {
             ArrayList<Person> persons = new ArrayList<>();
-            persons.add(new Person(phone,email));
-            entries.put(name,persons);
+            persons.add(new Person(phone, email));
+            entries.put(name, persons);
         }
     }
 
-    public ArrayList<String> getMails(String name) {
+
+    public List<String> getPerson(String name) {
         if (!entries.containsKey(name)) return null;
-        return entries.get(name).stream().map(person -> person.email).collect(Collectors.toCollection(ArrayList::new));
+//        return entries.get(name).stream().map(Person::getEmail).collect(Collectors.toList());
+        List<Person> persons = entries.get(name);
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < persons.size(); i++) {
+            result.add(persons.get(i).getPhone());
+            result.add(persons.get(i).getEmail());
+        }
+        return result;
     }
-
-
-
 
 }
